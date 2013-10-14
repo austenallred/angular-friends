f84392c50e3a47e5989598263977cc84d95abae5
ryanleeallred <ryanleeallred@gmail.com>
2013-10-14 14:15:03 -0600

    commits angular friends

--- FriendController.js
+++ FriendController.js
@@ -1,5 +1,22 @@
+friendsApp.controller('FriendController', function FriendController($scope, $http){
+    $http.get('https://s3.amazonaws.com/intuiplan_company_files/production/files/public/FriendData.json').success(function(response) {
+    	console.log(response)
+    	$scope.friends = response.results;
+    	$scope.searchName = '';
+    	$scope.searchLocation = '';
+    	$scope.sort1 = 'name';
+    	$scope.sort2 = 'false';
+
+   }); 
+    
+  
 
 
+    
+});
+       
 
+
+//$scope.$apply();
 
 
